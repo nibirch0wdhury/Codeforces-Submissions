af91bfe575df9c29fbddddd16f479a6c80bdbcb5
nibirch0wdhury <sadman.chowdhury747@gmail.com>
2026-02-14 13:42:34 +0600

    AUC's sheet#1, AB

---
+++ B_Basic_Data_Types.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a;
+    ll b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << '\n' << b << '\n' << c << '\n' << d << '\n' << e << '\n';
+}
