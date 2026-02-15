b3629b84b34d77083cb2a397d64fcf76bc52ec9a
nibirch0wdhury <sadman.chowdhury747@gmail.com>
2026-02-15 19:30:17 +0600

    AUC's sheet#1, C

---
+++ C_Simple_Calculator.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+
+int main(){
+    ll x, y;
+    cin >> x >> y;
+    cout << x << " + " << y << " = "<<x+y << '\n';
+    cout << x << " * " << y << " = "<<x*y << '\n';
+    cout << x << " - " << y << " = "<<x-y << '\n';
+}
